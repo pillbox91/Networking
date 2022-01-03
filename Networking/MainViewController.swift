a9d661a02484e111fb7c16c380cffc99d18b64e3
Andrey Averyanov <averyanov1996@gmail.com>
2022-01-03 17:27:37 +0300

    Initial Commit

---
+++ Networking/MainViewController.swift
@@ -0,0 +1,43 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by Андрей Аверьянов on 03.01.2022.
+//
+
+import UIKit
+
+enum URLExamples: String {
+    case imageURL = "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg"
+    case exampleOne = "https://swiftbook.ru//wp-content/uploads/api/api_course"
+    case exampleTwo = "https://swiftbook.ru//wp-content/uploads/api/api_courses"
+    case exampleThree = "https://swiftbook.ru//wp-content/uploads/api/api_website_description"
+    case examoleFour = "https://swiftbook.ru//wp-content/uploads/api/api_missing_or_wrong_fields"
+}
+
+class MainViewController: UICollectionViewController {
+    
+    // MARK: UICollectionViewDataSource
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserActionCell
+        
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width - 48, height: 100)
+    }
+}
---
+++ Networking/UserActionCell.swift
@@ -0,0 +1,13 @@
+//
+//  UserActionCell.swift
+//  Networking
+//
+//  Created by Андрей Аверьянов on 03.01.2022.
+//
+
+import UIKit
+
+class UserActionCell: UICollectionViewCell {
+    
+    @IBOutlet var userActionLabel: UILabel!
+}
