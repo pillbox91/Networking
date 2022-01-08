4063849f81c3de52652e81b2029a6d986f771a7f
Andrey Averyanov <averyanov1996@gmail.com>
2022-01-08 12:18:59 +0300

    Final "Alamofire"

--- Networking/Course.swift
+++ Networking/Course.swift
@@ -10,12 +10,37 @@ struct Course: Codable {
     let imageUrl: String?
     let numberOfLessons: Int?
     let numberOfTests: Int?
+    let numberOfLessonsPost: String?
+    let numberOfTestsPost: String?
     
-    enum CodingKeys: String, CodingKey {
-        case name = "Name"
-        case imageUrl = "ImageUrl"
-        case numberOfLessons = "Number_of_lessons"
-        case numberOfTests = "Number_of_tests"
+    //    enum CodingKeys: String, CodingKey {
+    //        case name = "Name"
+    //        case imageUrl = "ImageUrl"
+    //        case numberOfLessons = "Number_of_lessons"
+    //        case numberOfTests = "Number_of_tests"
+    //    }
+    
+    init(courseData: [String : Any]) {
+        name = courseData["name"] as? String
+        imageUrl = courseData["imageUrl"] as? String
+        numberOfLessons = courseData["number_of_lessons"] as? Int
+        numberOfTests = courseData["number_of_tests"] as? Int
+        numberOfLessonsPost = courseData["number_of_lessons"] as? String
+        numberOfTestsPost = courseData["number_of_tests"] as? String
+    }
+    
+    static func getCourses(from value: Any) -> [Course]? {
+        guard let coursesData = value as? [[String : Any]] else {return []}
+        return coursesData.compactMap { Course(courseData: $0) }
+        
+//        var courses: [Course] = []
+//
+//        for courseData in coursesData {
+//            let course = Course(courseData: courseData)
+//            courses.append(course)
+//        }
+//
+//        return courses
     }
 }
 
--- Networking/CoursesViewController.swift
+++ Networking/CoursesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class CoursesViewController: UITableViewController {
     
@@ -70,10 +71,62 @@ extension CoursesViewController {
     }
     
     func alamofireGetButtonPressed() {
-        
+        AF.request(URLExamples.exampleTwo.rawValue)
+            .validate()
+            .responseJSON { dataResponse in
+                switch dataResponse.result {
+                case .success(let value):
+                    
+                    self.courses = Course.getCourses(from: value) ?? []
+                    
+//                    guard let coursesData = value as? [[String : Any]] else {return}
+//
+//                    for courseData in coursesData {
+//                        let course = Course(
+//                            name: courseData["name"] as? String,
+//                            imageUrl: courseData["imageUrl"] as? String,
+//                            numberOfLessons: courseData["number_of_lessons"] as? Int,
+//                            numberOfTests: courseData["number_of_tests"] as? Int
+//                        )
+//
+//                        self.courses.append(course)
+//                    }
+//
+//                    for courseData in coursesData {
+//                        let course = Course(courseData: courseData)
+//                        self.courses.append(course)
+//                    }
+                    
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                    
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
     
     func alamofirePostButtonPressed() {
+        let courseData = [
+            "name": "Networking",
+            "imageUrl": "https://swiftbook.ru//wp-content/uploads/sites/2/2018/08/notifications-course-with-background.png",
+            "numberOfLessonsPost": "18",
+            "numberOfTestsPost": "10"
+        ]
         
+        AF.request(URLExamples.postRequest.rawValue, method: .post, parameters: courseData)
+            .validate()
+            .responseDecodable(of: Course.self) { dataResponse in
+                switch dataResponse.result {
+                case .success(let course):
+                    self.courses.append(course)
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
 }
